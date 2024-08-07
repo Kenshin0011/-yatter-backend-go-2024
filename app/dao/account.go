1d544afdfac41404b85fe548ee7c9c2f892d5efb
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-08-07 12:19:08 +0900

    FindAccountByUsername実装

--- app/dao/account.go
+++ app/dao/account.go
@@ -25,7 +25,16 @@ func NewAccount(db *sqlx.DB) *account {
 	return &account{db: db}
 }
 
-// FindByUsername : ユーザ名からユーザを取得
+func (a *account) Create(ctx context.Context, tx *sqlx.Tx, acc *object.Account) error {
+	_, err := a.db.Exec("insert into account (username, password_hash, display_name, avatar, header, note, create_at) values (?, ?, ?, ?, ?, ?, ?)",
+		acc.Username, acc.PasswordHash, acc.DisplayName, acc.Avatar, acc.Header, acc.Note, acc.CreateAt)
+	if err != nil {
+		return fmt.Errorf("failed to insert account: %w", err)
+	}
+
+	return nil
+}
+
 func (a *account) FindByUsername(ctx context.Context, username string) (*object.Account, error) {
 	entity := new(object.Account)
 	err := a.db.QueryRowxContext(ctx, "select * from account where username = ?", username).StructScan(entity)
@@ -38,14 +47,4 @@ func (a *account) FindByUsername(ctx context.Context, username string) (*object.
 	}
 
 	return entity, nil
-}
-
-func (a *account) Create(ctx context.Context, tx *sqlx.Tx, acc *object.Account) error {
-	_, err := a.db.Exec("insert into account (username, password_hash, display_name, avatar, header, note, create_at) values (?, ?, ?, ?, ?, ?, ?)",
-		acc.Username, acc.PasswordHash, acc.DisplayName, acc.Avatar, acc.Header, acc.Note, acc.CreateAt)
-	if err != nil {
-		return fmt.Errorf("failed to insert account: %w", err)
-	}
-
-	return nil
 }
