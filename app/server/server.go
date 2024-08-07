fc8a22cfe981c60e745683396314b212b4dc65de
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-08-07 15:46:40 +0900

    cancelを入れた

--- app/server/server.go
+++ app/server/server.go
@@ -52,7 +52,9 @@ func Run() error {
 	}()
 
 	<-ctx.Done()
-	ctx, _ = context.WithTimeout(context.Background(), time.Second*5)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
 	if err := srv.Shutdown(ctx); err != nil {
 		log.Fatal(err)
 	}
